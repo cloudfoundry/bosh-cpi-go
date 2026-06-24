bb8f78dd274a0b2451e3a21a0b6fd8dbe0bcb522
I761617 <ivaylo.ivanov06@sap.com>
2026-06-24 14:13:14 +0300

    Add UpdateDisk functionality to action factory and CPI

--- docs/example.go
+++ docs/example.go
@@ -120,6 +120,10 @@ func (c CPI) ResizeDisk(cid apiv1.DiskCID, size int) error {
 	return nil
 }
 
+func (c CPI) UpdateDisk(cid apiv1.DiskCID, size int, props apiv1.DiskCloudProps) (interface{}, error) {
+	return cid.AsString(), nil
+}
+
 func (c CPI) SnapshotDisk(cid apiv1.DiskCID, meta apiv1.DiskMeta) (apiv1.SnapshotCID, error) {
 	return apiv1.NewSnapshotCID("snap-cid"), nil
 }
